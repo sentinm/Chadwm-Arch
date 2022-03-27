995c97c65b0cdfc605b54c949d0801c48c15d8cb
Lexterd <79321771+sentinm@users.noreply.github.com>
2022-03-27 11:40:06 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,4 +3,5 @@
 rm -rf ~/.config/chadwm
 rm -rf ~/Pictures/pink_cat.png
 rm -rf /usr/share/xsessions/chadwm.desktop
+rm -rf ~/.local/share/fonts/material* && rm -rf ~/.local/share/fonts/Material*
 echo "done! - Uninstallation" 
