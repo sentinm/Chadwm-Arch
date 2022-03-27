4b406a06658c0162ab493a660beaa86ac38b1c32
Lexterd <79321771+sentinm@users.noreply.github.com>
2022-03-27 08:10:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@
 
 #pacman -S picom
 #pacman -S rofi
+#pacman -S xorg-xbacklight
+#pacman -S xorg-xrdb
 pacman -S imlib2
-pacman -S xorg-xsetroot
+#pacman -S xorg-xsetroot
 pacman -S dash
 pacman -S nerd-fonts-jetbrains-mono
 
@@ -16,12 +18,15 @@ pacman -S nerd-fonts-jetbrains-mono
 # Installation
 
 cd ~/.config && git clone https://github.com/sentinm/Chadwm-Arch.git chadwm
-ech "done! - Fetching chadwm repository.
+ech "done! - Fetching chadwm repository." 
 
 cp ~/.config/chadwm/fonts/* ~/.local/share/fonts/
-echo "done! - Copying font files.
+echo "done! - Copying font files." 
+
+curl -o ~/Pictures/pink_cat.png https://github.com/catppuccin/wallpapers/raw/main/minimalistic/pink-cat.png 
+echo "done! - Loading wallpaper." 
 
 mv ~/.config/chadwm/chadwm.desktop /usr/share/xsessions/
-echo "done! - Creating desktop entry for chadwm.
+echo "done! - Creating desktop entry for chadwm."
 
 echo "done! - Installation" 
