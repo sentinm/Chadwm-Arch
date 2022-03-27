15e467c7f87beb5f16c6c2f0c3bc917459e0fb84
Lexterd <79321771+sentinm@users.noreply.github.com>
2022-03-27 07:56:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+# Installing required packages
+
+#pacman -S picom
+#pacman -S rofi
+pacman -S imlib2
+pacman -S xorg-xsetroot
+pacman -S dash
+pacman -S nerd-fonts-jetbrains-mono
+
+#yay -S polybar
+#yay -S eww-git
+#yay -S dwm
+
+# Installation
+
+cd ~/.config && git clone https://github.com/sentinm/Chadwm-Arch.git chadwm
+ech "done! - Fetching chadwm repository.
+
+cp ~/.config/chadwm/fonts/* ~/.local/share/fonts/
+echo "done! - Copying font files.
+
+mv ~/.config/chadwm/chadwm.desktop /usr/share/xsessions/
+echo "done! - Creating desktop entry for chadwm.
+
+echo "done! - Installation" 
